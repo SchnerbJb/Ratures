66e76fb75efc61397ff3cbc317a9259d93ccbb07
SchnerbJb <j.schnerb@gmail.com>
2022-10-13 22:22:01 +0200

    id page finished

--- pages/book/[bid].tsx
+++ pages/book/[bid].tsx
@@ -1,5 +1,31 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { prisma } from "../../lib/prisma";
+import styles from '../../styles/Home.module.css'
 
-const BookId = () => {
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+    const book = await prisma.book.findUnique({
+        where: {
+            id: Number(params?.bid),
+        },
+    });
+    return {
+        props: {
+            book,
+        }
+    };
+};
+
+
+const BookId = ({ book }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+
+    return (
+
+        <div className={styles.card} key={book.id}>
+            <h2>{book.title}</h2>
+            <h4>By {book.author}</h4>
+            <p>{book.synopsis}</p>
+            <p className={styles.rating}>{book.rating} / 5</p>
+        </div>
+    )
 }
-
 export default BookId;
